21f4f64ad03eec5c73fc662421a6f109f2b78722
Tony Uteev <uteev@bk.ru>
2017-03-23 19:11:14 +0300

    balls change color 23 03

--- src/main/java/uteevbkru/Ball.java
+++ src/main/java/uteevbkru/Ball.java
@@ -1,18 +1,49 @@
 package uteevbkru;
 
+import java.awt.Color;
 import java.awt.geom.*;
+import java.util.ArrayList;
 
 public class Ball {
 	private static int count_ball = 0;
+	private static int midle_ball = 0;
 	private static final int XS = 15;	
 	private static final int YS = 15;
-	private double x = 0 + count_ball*20;
+	private double x = count_ball*20;
 	private double y = 0;
 	private double dx = 0.1;
 	private double dy = 0.1;
-	//TODO color!!!!!
+	
+	private ArrayList<Color> colors = new ArrayList<Color>(); 	
+	private int color;
 	public Ball(){
 		count_ball++;
+		colors.add(Color.blue);
+		colors.add(Color.cyan);
+		colors.add(Color.gray);
+		colors.add(Color.red);
+		colors.add(Color.pink);
+		colors.add(Color.red);
+		colors.add(Color.darkGray);
+		colors.add(Color.magenta);
+		colors.add(Color.orange);
+		colors.add(Color.white);		
+		color = (int) (Math.random()*colors.size());
+		
+		boolean abc = true;
+		if(x < 400){			
+			midle_ball = count_ball;
+		}
+		else{			
+			System.out.println("SHOPA"+" "+ midle_ball + " " + count_ball);			
+			x  = x - ((count_ball - midle_ball)*30);			
+			if(x < 10){								
+				x = count_ball*20;
+			}
+		}
+	}
+	public Color getColor(){		
+		return colors.get(color);
 	}
 	
 	public void move(Rectangle2D b){
--- src/main/java/uteevbkru/BallComponent.java
+++ src/main/java/uteevbkru/BallComponent.java
@@ -7,10 +7,9 @@ import javax.swing.*;
 public class BallComponent extends JPanel{
 
 	private static final int DEF_WID = 450;
-	private static final int DEF_HEI = 350;
+	private static final int DEF_HEI = 350;	
 	
-	private java.util.List<Ball> balls = new ArrayList<Ball>();
-	
+	private java.util.List<Ball> balls = new ArrayList<Ball>();		
 	public void add(Ball a){
 		balls.add(a);	
 	}
@@ -18,8 +17,8 @@ public class BallComponent extends JPanel{
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		for(Ball b : balls){			
-			Paint p = Color.CYAN;
+		for(Ball b : balls){				
+			Paint p = b.getColor();
 			g2.setPaint(p);
 			g2.fill(b.getShape());//было draw!!
 		}		
