04a4c0e9068699ccf8819931f6b4259fc96008d6
Macbook <macbook@MBP-Macbook.Dlink>
2021-02-18 12:28:34 +0300

    delete some

--- src/main/java/uteevbkru/Bounce.java
+++ src/main/java/uteevbkru/Bounce.java
@@ -19,28 +19,4 @@ public class Bounce {
 				}
 			});
 	}
-
-//	public static void main(String[] args) {
-//		String name = "мама мыла раму";
-//		StringBuilder finalName = new StringBuilder();
-//
-//		for(int i = 0; i < name.length(); i++) {
-//			if(i==0) {
-//				finalName.append(name.substring(0,1).toUpperCase());
-//				i++;
-//			}
-//
-//			String prom = name.substring(i,i+1);
-//			if(prom.equals(" ")) {
-//				finalName.append(" ");
-//				String upper = name.substring(i+1,i+2).toUpperCase();
-//				finalName.append(upper);
-//				i++;
-//			} else {
-//				finalName.append(prom);
-//			}
-//		}
-//		System.out.println(finalName);
-//	}
-	
 }
